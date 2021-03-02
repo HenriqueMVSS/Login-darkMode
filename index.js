06a2223b3cb60149b5e85c2bd80f6c83b2d7baf8
Henrique Mateus Vieira dos S. Silva <henrique.silva@fbest.org.br>
2021-03-02 12:17:46 -0300

    Login screen with dark mode

---
+++ index.js
@@ -0,0 +1,11 @@
+function darkMode()  {
+   let darkMode = document.getElementById('Teste')
+   darkMode.classList.add('dark-mode')
+
+}
+
+function removeDarkMode() {
+    let darkMode = document.getElementById('Teste')
+    darkMode.classList.remove('dark-mode')
+
+}
